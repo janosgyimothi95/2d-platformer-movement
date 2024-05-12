107e19a1e53b8eed2759a108dfee16c16b21b52a
Gyimóth János <janos.gyimothi@gmail.com>
2024-05-12 11:02:22 +0200

    Basic compoenent based movement

---
+++ scenes/components/animation_component.gd
@@ -0,0 +1,27 @@
+class_name AnimationComponent
+extends Node
+
+@export_subgroup("Nodes")
+@export var sprite: AnimatedSprite2D
+
+func handle_horizontal_flip(move_dir: float) -> void:
+	if move_dir == 0:
+		return
+	
+	sprite.flip_h = false if move_dir > 0 else true
+	
+	
+func handle_move_animation(move_dir: float) -> void:
+	handle_horizontal_flip(move_dir)
+	
+	if move_dir != 0:
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+
+
+func handle_jump_animation(is_jumping: bool, is_falling: bool) -> void:
+	if is_jumping:
+		sprite.play("jump")
+	elif is_falling:
+		sprite.play("fall")
